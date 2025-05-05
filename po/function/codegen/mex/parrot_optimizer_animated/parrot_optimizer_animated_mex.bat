462b2c3770bb0def7f01c3cbb97a538f21c1e76a
luyu14039 <1403975281@qq.com>
2025-05-05 12:46:42 +0800

    需要暂时删除

--- po/function/codegen/mex/parrot_optimizer_animated/parrot_optimizer_animated_mex.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call setEnv.bat
-"D:\Matlab\fulll\toolbox\shared\coder\ninja\win64\ninja.exe" -t compdb cc cxx cudac > compile_commands.json
-"D:\Matlab\fulll\toolbox\shared\coder\ninja\win64\ninja.exe" -v %*
--- po/function/initialization.m
+++
@@ -1,4 +0,0 @@
-function X = initialization(N, dim, ub, lb)
-    % 生成均匀分布的初始种群
-    X = rand(N, dim).*(ub - lb) + lb;
-end
